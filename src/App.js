c4f6bfeb8c87f8790b04b9a4dc7cc92ca8b019a7
Jaime-Ginesky <jaimelee79@gmail.com>
2023-07-05 19:55:08 -0700

    Browser router and compiled routing structure

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
   <Routes>
     <Route path='/' element={<Navigation />}>
-     <Route index element={Home />} />
+     <Route index element={<Home /> } />
     </Route>
   </Routes>
  
