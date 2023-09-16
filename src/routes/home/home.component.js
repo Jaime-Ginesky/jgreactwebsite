55d398aba37ae1d3595b57dabbcae904be4d3062
Jaime-Ginesky <jaimelee79@gmail.com>
2023-09-16 14:55:23 -0700

    slide down styling

--- src/routes/home/home.component.js
+++ src/routes/home/home.component.js
@@ -2,20 +2,19 @@ import { Outlet } from 'react-router-dom';
 import { Fragment } from 'react';
 import ProjectMenu from '../../components/projects/projects.component';
 
-import { MainContainer, Grid, SlideDown } from './home.styles';
+import { MainContainer, SlideDown } from './home.styles';
 
 const Home = () => {
 return(
   <Fragment>
     <MainContainer>
-      <Grid>
+      
         <h3>Hello, My Name is</h3>
         <h1>JaimeGinesky</h1>
         <SlideDown>
           <h2>Front-end Developer based in Los Angeles, CA</h2>
           </SlideDown>
         <footer>jfdh</footer>
-        </Grid>
      <ProjectMenu />
      <Outlet />
      </MainContainer>
--- src/routes/home/home.styles.js
+++ src/routes/home/home.styles.js
@@ -10,7 +10,7 @@ import styled from 'styled-components';
   // height: 100vh;
   // width: 100vw;
   // background: var(--background-color);
-
+  
 
      h3 {
         display: flex;
@@ -24,6 +24,11 @@ import styled from 'styled-components';
      }
    
      h1 {
+      display: grid;
+      grid-template-columns: {1};
+      grid-gap: 10px;
+      justify-content: center;
+      align-content: center;
        color: var(--image-text-color);
        opacity: 80%;
        font-size: 150px;
@@ -31,41 +36,46 @@ import styled from 'styled-components';
        font-family: "Poppins", sans-serif;
        padding-top: 80px;
       }
-    h2 {
-       display: flex;
-       justify-content: center;
-       align-content: center;
-       position: fixed;
-       color:steelblue;
-       opacity: 60%;
-       font-size: 24px;
-       font-family: "Cormorant Garamond", serif;
-       padding-bottom: 30px;
-     }
+    // h2 {
+    //    display: grid;
+    //    justify-content: center;
+    //    align-content: center;
+    //    grid-gap: 10px;
+    //    position: fixed;
+    //    color:steelblue;
+    //    opacity: 60%;
+    //    font-size: 24px;
+    //    font-family: "Cormorant Garamond", serif;
+    //    margin: -90px auto;
+    //  }
     `
 
-  export const Grid = styled.div`
-    display: Grid;
-    grid-template-columns: {1};
-    grid-gap: 10px;
-   `;
 
    export const SlideDown = styled.div `
    
     width:100%;
     overflow:hidden;
     margin-left: 450px;
-    max-width:500px;
+    max-width: 530px;
+    color:steelblue;
+    opacity: 50%;
+    margin: -120px auto;
+    display: grid;
+    justify-content: center;
+    align-content: center;
+    grid-gap: 5px;
+  
 
   
    h2 {
     text-align: center;
     animation:2s slide-down 2s forwards;
-       transform:translateY(-100%);
+       transform:translateY(-170%);
     @keyframes slide-down {
     to {
       transform:translateX(0%);
     }
   }
+}
    `
 
